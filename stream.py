309f8baf47f91319b0b71afb88d03feb5c1e8f9f
offendo <nilaypatel2@gmail.com>
2017-07-10 17:38:50 -0400

    now has scheduled data grab and interday data grab

--- stream.py
+++ stream.py
@@ -1,6 +1,6 @@
 import gdax, time
 import datetime as dt
-
+import multiprocessing as mp
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,11 +13,11 @@ class webclient(gdax.WebsocketClient):
 
     def __init__(self, url="wss://ws-feed.gdax.com", products=None, message_type="subscribe"):
         self.url = url
-        self.products = products
+        self.products = input('Enter products: ').split(',')
         self.type = message_type
         self.stop = False
         self.ws = None
-        self.thread = None        
+        self.thread = None
         self.data = messages()
 
 
@@ -28,15 +28,15 @@ class webclient(gdax.WebsocketClient):
         if len(self.data.typedict[msg_type]) == 0:
             self.data.create_frame(msg_type, msg)
         else:
-            self.data.typedict[msg_type] = self.data.queuer(msg, self.data.typedict[msg_type], 100)
+            self.data.typedict[msg_type] = self.data.queuer(msg, self.data.typedict[msg_type], 1000)
             # print(self.data.typedict[msg_type])
-        # time.sleep(1)
+        # time.sleep(.2)
 
     def get_messages(self):
         return self.data
 
 
-class messages: 
+class messages:
 
     def __init__(self):
         self.received_frame              = pd.DataFrame()
@@ -74,13 +74,18 @@ class messages:
         return frame
 
 
-    def animate(self, axis, frame):
+    def animate(self, axis, frame, ani_min_price, ani_max_price, color):
         prices = [float(x) for x in frame['price']]
         times = frame['time']
         axis.clear()
-        axis.set_ylim([2300,2700])
+
+        axis.set_ylim([ani_min_price, ani_max_price])
+
         axis.set_autoscaley_on(False)
-        axis.plot_date(times, prices, xdate=True, ydate=False, color='blue', linestyle='solid', linewidth=2,  markersize=2)
+        axis.set_xlabel('time', fontsize=10)
+        axis.set_ylabel('prices', fontsize=10)
+        axis.yaxis.grid(color='black', linestyle='-', linewidth=1)
+        axis.plot_date(times, prices, xdate=True, ydate=False, color=color, linestyle='solid', linewidth=1,  markersize=1)
 
 
         # based on message type, will return respective dataframe
@@ -88,29 +93,72 @@ class messages:
         return self.typedict[msg_type]
 
 
-def create_graph(socket, msg_type):
+def create_graph(socket, msg_type, min_price=0, max_price=3000, color='blue'):
     while len(socket.get_messages().get_frame('done')) < 5:
         time.sleep(1)  # wait for data to be put into this frame
 
     # graph
     fig = plt.figure()
+    fig.suptitle(socket.products[0] + ' ' + msg_type, fontsize=15)
+
     ax1 = fig.add_subplot(1,1,1)
 
     # updates graph every interval
-    param_animate = lambda x: socket.get_messages().animate(ax1, socket.get_messages().get_frame(msg_type))
+    param_animate = lambda x: socket.get_messages().animate(ax1, socket.get_messages().get_frame(msg_type),
+                                        min_price, max_price, color)
     animation = ani.FuncAnimation(fig, param_animate, interval=10)
     plt.show()
 
+
+class historic_data(gdax.PublicClient):
+    def daterange(self, start_date, end_date):
+        for n in range(int ((end_date - start_date).days + 1)):
+            yield start_date + dt.timedelta(days=n)
+
+    def interday_data(self, start_date, end_date, product, output):
+        # NOTE: these dates are inclusive
+        start = dt.datetime.strptime(start_date, '%Y-%m-%d')    # start
+        end = dt.datetime.strptime(end_date, '%Y-%m-%d')        # end
+
+        with open(output, 'a') as f:
+            f.write('time,low,high,open,close,volume\n')            # titles
+
+        for d in self.daterange(start, end):
+            data = self.get_product_historic_rates(product, d.isoformat(), \
+            (d + dt.timedelta(days=1)).isoformat(), '300')
+            print(d)
+            with open(output, 'a') as f:
+                data = [[str((dt.datetime(1970, 1, 1, 0, 0, 0) + dt.timedelta(seconds=row[0])))] + \
+                        [str(item) for item in row[1:]] for row in data]
+
+                [f.write(','.join(line) + '\n') for line in reversed(data)]
+    def scheduled_pull(self, interval, granularity, products):
+        if not isinstance(products, list):
+            products = [products]
+
+        for p in products:
+            output_end = p + '_' + (dt.datetime.now()-dt.timedelta(hours=interval)).strftime('%Y-%m-%dT%H:%M:%S')
+            output_end = output_end.replace(':', '').replace('-', '')
+            outfile = 'M:\\nilay\\criptoe\\historical_data\\%s.csv' % output_end
+
+            with open(outfile, 'a') as f:
+                f.write('time,low,high,open,close,volume\n')            # titles
+
+            data = self.get_product_historic_rates(p, (dt.datetime.now()-dt.timedelta(hours=interval, minutes=2)).isoformat(), dt.datetime.now().isoformat(), str(60*granularity))
+            with open(outfile, 'a') as f:
+                data = [[str((dt.datetime(1970, 1, 1, 0, 0, 0) + dt.timedelta(seconds=int(row[0]))))] + \
+                [str(item) for item in row[1:]] for row in data]
+                [f.write(','.join(line) + '\n') for line in reversed(data)]
+
+
 def main():
-    ws = webclient() # open stream
-    ws.start()
-
-    create_graph(ws, 'done')
-    # x = input()
-    # while (x != 'q'): # stall to close socket
-    #     x = input()
-
-    ws.close()    # close stream
+    hd = historic_data()
+    product = 'BTC-USD'
+    hd.scheduled_pull(2, 1, product)
+    # ws1 = webclient() # open stream
+    # ws1.start()
+    # create_graph(ws1, 'done')
+    # ws1.close()
 
 
 if __name__ == '__main__':
