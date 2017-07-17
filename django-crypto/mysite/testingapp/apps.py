02ab6bdec4dc7b95dd9a21be6d2e784427e5c809
niral patel <the1mightypan@gmail.com>
2017-07-16 23:41:04 -0400

    removed django-crypto folder

--- django-crypto/mysite/testingapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestingappConfig(AppConfig):
-    name = 'testingapp'
--- django-crypto/mysite/testingapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import get_object_or_404, render
-
-
-def testview(request):
-    return render(request, 'testingapp/testview.html')
